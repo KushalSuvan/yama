f950a9fba69409d8897cde86b65a19824ef9a281
KushalSuvan <kushalsuvanjenamani@gmail.com>
2025-05-05 10:56:05 +0530

    chore: expose train_model of extractive_summarizer

--- src/extractive_summarizer/__init__.py
+++ src/extractive_summarizer/__init__.py
@@ -1,5 +1,6 @@
 from extractive_summarizer.dataset import ExtractiveSummarizationDataset
 from extractive_summarizer.config import get_config
 from extractive_summarizer.model import ExtractiveSummarizer
+from extractive_summarizer.train import train_model
 
 __all__ = ["ExtractiveSummarizationDataset", "get_config", "ExtractiveSummarizer"]
